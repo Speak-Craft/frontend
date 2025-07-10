ce7e0a1e06efa0b945686d9fc36e0b76fddcd326
VISHARADDS <it21822544@my.sliit.lk>
2025-07-10 12:32:37 +0530

    Login Frontend component

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import { motion } from "framer-motion";
+import { FaMicrophone, FaStop, FaPlay, FaPause, FaClock } from "react-icons/fa";
+import GaugeChart from "react-gauge-chart";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+// import jsPDF from "jspdf";
+// import * as htmlToImage from "html-to-image";
+
+// import image01 from "/assets/images/image01.png";
+
+const Login = () => {
+  
+
+  return (
+    <div className="absolute top-[4rem] left-64 w-[calc(100%-17rem)]  p-8 flex justify-center items-center">
+
+    </div>
+  );
+};
+
+export default Login;
